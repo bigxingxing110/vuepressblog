3a0049d249449b88a16b4ac7078b4bf4d7410723
bigxingxing110 <545346965@qq.com>
2020-09-28 13:07:43 +0800

    博客初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    title: '徐宇星的博客',
+    head: [ // 注入到当前页面的 HTML <head> 中的标签
+        ['link', {
+            rel: 'icon',
+            href: '/01.png'
+        }], // 增加一个自定义的 favicon(网页标签的图标)
+    ],
+    themeConfig: {
+        logo: '/01.png', // 左上角logo
+        nav: [ // 导航栏配置
+            {
+                text: '首页',
+                link: '/'
+            },
+            {
+                text: '技术文档',
+                link: '/tech/interview/'
+            },
+            {
+                text: '简书主页',
+                link: 'https://www.jianshu.com/u/c455567c7f50'
+            }
+        ],
+        sidebar: 'auto', // 侧边栏配置
+    }
+};
